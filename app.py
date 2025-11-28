30c6719f7a06f81b06f820c4c13bfdc1882a6694
voyegerz <nirajkumaruiflow@gmail.com>
2025-11-28 14:17:26 +0530

    Ready for deployment

--- app.py
+++ app.py
@@ -192,4 +192,5 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    # This is for local development only
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
